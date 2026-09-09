730bc33febdb4a76aaa1ca8f0b031b563a55d9df
ajayn3300 <ajayn3300@gmail.com>
2026-09-09 22:14:59 +0530

    added Youtube comments summarizer MCP server

---
+++ YT_cmnts_MCP.py
@@ -0,0 +1,18 @@
+from youtube_comment_downloader import YoutubeCommentDownloader
+from itertools import islice
+from mcp.server.fastmcp import FastMCP
+
+#intialize server
+mcp = FastMCP('YT_CMNTS')
+
+@mcp.tool()
+def yt_cmnt_downloader(link: str) -> list:
+    '''this function takes youtube link as an argument and return a list including maximum 200 comments on that video'''
+    downloader = YoutubeCommentDownloader()
+    comments = downloader.get_comments_from_url(link)
+    comments = [comment['text'] for comment in islice(comments, 200)]
+    return comments
+
+
+if __name__ == "__main__":
+    mcp.run(transport="stdio")
--- chatbot.py
+++ chatbot.py
@@ -17,8 +17,9 @@ from langchain_mcp_adapters.client import MultiServerMCPClient
 llm  = ChatGroq(model="openai/gpt-oss-20b")
 
 # intializing client
-client = MultiServerMCPClient({'STOCKS' : {'transport':'stdio', 'command':'C:/Users/ajayn/AppData/Local/Programs/Python/Python311/python.exe', 'args':['D:/WORK/MCP/mcp_server.py']}})
-
+client = MultiServerMCPClient({
+    'STOCKS' : {'transport':'stdio', 'command':'C:/Users/ajayn/AppData/Local/Programs/Python/Python311/python.exe', 'args':['D:/WORK/MCP/stock_MCP.py']},
+    'YT_CMNTS' : {'transport':'stdio', 'command':'C:/Users/ajayn/AppData/Local/Programs/Python/Python311/python.exe', 'args':['D:/WORK/MCP/YT_cmnts_MCP.py']}})
 
 # modifying the build_graph function
 async def build_graph():
@@ -50,7 +51,7 @@ async def main():
     chatbot = await build_graph()
 
     #invoke chatbot
-    res = await chatbot.ainvoke({'messages':HumanMessage('what is the current stock price of apple ?')})
+    res = await chatbot.ainvoke({'messages':HumanMessage('this is the youtube video link of a product, tell me all the goods and bads about this product,  what peoples are saying by reading the comments on this video link : https://www.youtube.com/watch?v=h3M9phIriT4 ')})
 
     print(res['messages'][-1].content)
 
